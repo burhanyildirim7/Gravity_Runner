f6fe0ed582e343029bb47574a3a1395f292fe8a4
burhanyildirim7 <burhanyldrm07@gmail.com>
2022-03-29 12:36:04 +0300

    TF Güncellemesi.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,15 @@ using DG.Tweening;
 
 public class PlayerController : MonoBehaviour
 {
-	#region SINGLETON
-	public static PlayerController instance;
+    #region SINGLETON
+    public static PlayerController instance;
     private void Awake()
     {
         if (instance == null) instance = this;
         else Destroy(this);
     }
-	#endregion
-	public int collectibleDegeri;
+    #endregion
+    public int collectibleDegeri;
     public bool xVarMi = true;
     public bool collectibleVarMi = true;
     private bool isEnableForSwipe;
@@ -30,10 +30,10 @@ public class PlayerController : MonoBehaviour
     public GameObject model, cameraTarget;
     public GameObject level1OnBoard, level21OnBoard, level22OnBoard, level31OnBoard, level32OnBoard, level33OnBoard;
     public int LevelPlatformCount = 6;
-    [HideInInspector]public Animator PlayerAnimator;
+    [HideInInspector] public Animator PlayerAnimator;
     public GameObject playerModel1, playerModel2;
     public GameObject coinPrefab, coinTarget;
-    public GameObject tozEfekti, playerModel1Ayak,playerModel2Ayak;
+    public GameObject tozEfekti, playerModel1Ayak, playerModel2Ayak;
     public GameObject coinEfekti;
     private int levelCoinCount;
 
@@ -95,17 +95,17 @@ public class PlayerController : MonoBehaviour
             lastMousePosX = Input.mousePosition.x - screenWidth;
             lastMousePosY = Input.mousePosition.y - screenHeight;
 
-            if((lastMousePosY - firstMousePosY) > swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit / 2) // RT
-			{
+            if ((lastMousePosY - firstMousePosY) > swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit / 2) // RT
+            {
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveRT();
-			}
-            else if ((lastMousePosY - firstMousePosY) <- swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit / 2) // RB
+            }
+            else if ((lastMousePosY - firstMousePosY) < -swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit / 2) // RB
             {
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveRB();
             }
-            else if ((lastMousePosY - firstMousePosY) < -swipeControlLimit && (lastMousePosX - firstMousePosX) <- swipeControlLimit / 2) // LB
+            else if ((lastMousePosY - firstMousePosY) < -swipeControlLimit && (lastMousePosX - firstMousePosX) < -swipeControlLimit / 2) // LB
             {
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveLB();
@@ -120,11 +120,11 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveT();
             }
-            else if ((lastMousePosY - firstMousePosY) <- swipeControlLimit && Mathf.Abs(lastMousePosX - firstMousePosX) < swipeControlLimit / 2) // B
+            else if ((lastMousePosY - firstMousePosY) < -swipeControlLimit && Mathf.Abs(lastMousePosX - firstMousePosX) < swipeControlLimit / 2) // B
             {
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveB();
-            }     
+            }
         }
         else if (Input.GetMouseButton(0) && isEnableForSwipe && LevelPlatformCount == 4)
         {
@@ -141,7 +141,7 @@ public class PlayerController : MonoBehaviour
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveB();
             }
-            else if (Mathf.Abs(lastMousePosY - firstMousePosY) < swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit ) // R
+            else if (Mathf.Abs(lastMousePosY - firstMousePosY) < swipeControlLimit && (lastMousePosX - firstMousePosX) > swipeControlLimit) // R
             {
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveR();
@@ -164,7 +164,7 @@ public class PlayerController : MonoBehaviour
             }
             else if ((lastMousePosY - firstMousePosY) < -swipeControlLimit) // B
             {
-                
+
                 StartCoroutine(DlayAndEnabledSwipe());
                 MoveB();
             }
@@ -188,27 +188,27 @@ public class PlayerController : MonoBehaviour
     }
 
 
-	#region MOVEMENTS
-	private void MoveT()
-	{
+    #region MOVEMENTS
+    private void MoveT()
+    {
         JumpAnim();
         isEnableForSwipe = false;
-        cameraTarget.transform.DOLocalMove(new Vector3(0,6f,0),.5f);
-        transform.DOLocalMove(new Vector3(0,10,0),.5f);
-        transform.DORotate(new Vector3(0,0,180),.5f);
-	}
+        cameraTarget.transform.DOLocalMove(new Vector3(0, 6f, 0), .5f);
+        transform.DOLocalMove(new Vector3(0, 10, 0), .5f);
+        transform.DORotate(new Vector3(0, 0, 180), .5f);
+    }
 
     private void MoveB()
-	{
+    {
         JumpAnim();
         isEnableForSwipe = false;
         cameraTarget.transform.DOLocalMove(new Vector3(0, 4f, 0), .5f);
         transform.DOLocalMove(Vector3.zero, .5F);
-        transform.DORotate(new Vector3(0, 0,0), .5f);
+        transform.DORotate(new Vector3(0, 0, 0), .5f);
     }
 
     private void MoveR()
-	{
+    {
         JumpAnim();
         isEnableForSwipe = false;
         cameraTarget.transform.DOLocalMove(new Vector3(1f, 5, 0), .5f);
@@ -217,7 +217,7 @@ public class PlayerController : MonoBehaviour
     }
 
     private void MoveL()
-	{
+    {
         JumpAnim();
         isEnableForSwipe = false;
         cameraTarget.transform.DOLocalMove(new Vector3(-1f, 5, 0), .5f);
@@ -260,9 +260,9 @@ public class PlayerController : MonoBehaviour
         transform.DORotate(new Vector3(0, 0, 60), .5f);
     }
 
-	#endregion 
-	
-  
+    #endregion
+
+
 
     /// <summary>
     /// Playerin collider olaylari.. collectible, engel veya finish noktasi icin. Burasi artirilabilir.
@@ -279,30 +279,30 @@ public class PlayerController : MonoBehaviour
             MoreMountains.NiceVibrations.MMVibrationManager.Haptic(MoreMountains.NiceVibrations.HapticTypes.MediumImpact);
             if (playerModel1.activeInHierarchy) Instantiate(coinEfekti, playerModel1Ayak.transform.position, Quaternion.identity);
             else if (playerModel2.activeInHierarchy) Instantiate(coinEfekti, playerModel2Ayak.transform.position, Quaternion.identity);
-            MoreMountains.NiceVibrations.MMVibrationManager.Haptic(MoreMountains.NiceVibrations.HapticTypes.MediumImpact);
+            //MoreMountains.NiceVibrations.MMVibrationManager.Haptic(MoreMountains.NiceVibrations.HapticTypes.MediumImpact);
             Destroy(other.gameObject);
-            GameObject obj = Instantiate(coinPrefab, transform.position, Quaternion.Euler(new Vector3(90,0,0)));
-            obj.transform.DOMove(coinTarget.transform.position,.5f).OnComplete(()=> { Destroy(obj); });
+            GameObject obj = Instantiate(coinPrefab, transform.position, Quaternion.Euler(new Vector3(90, 0, 0)));
+            obj.transform.DOMove(coinTarget.transform.position, .5f).OnComplete(() => { Destroy(obj); });
             obj.transform.DOScale(Vector3.one * .2f, .48f);
             GameController.instance.SetScore(collectibleDegeri); // ORNEK KULLANIM detaylar icin ctrl+click yapip fonksiyon aciklamasini oku
-           // GameObject arti = Instantiate(artiBir, new Vector3(model.transform.position.x, model.transform.position.y + 5, model.transform.position.z), Quaternion.identity, model.transform);
-           
+                                                                 // GameObject arti = Instantiate(artiBir, new Vector3(model.transform.position.x, model.transform.position.y + 5, model.transform.position.z), Quaternion.identity, model.transform);
+
         }
         else if (other.CompareTag("engel"))
         {
             // ENGELELRE CARPINCA YAPILACAKLAR....
             MoreMountains.NiceVibrations.MMVibrationManager.Haptic(MoreMountains.NiceVibrations.HapticTypes.MediumImpact);
-            if(levelCoinCount>= 5)
-			{
+            if (levelCoinCount >= 5)
+            {
                 levelCoinCount -= 5;
                 CoinCrashEffects();
-			}
-            else if(levelCoinCount < 5)
-			{
+            }
+            else if (levelCoinCount < 5)
+            {
                 GameOverEvents();
-			}
-           // GameObject eksi = Instantiate(eksiBir, new Vector3(model.transform.position.x, model.transform.position.y + 3, model.transform.position.z), Quaternion.identity, model.transform);
-    
+            }
+            // GameObject eksi = Instantiate(eksiBir, new Vector3(model.transform.position.x, model.transform.position.y + 3, model.transform.position.z), Quaternion.identity, model.transform);
+
             GameController.instance.SetScore(-collectibleDegeri); // ORNEK KULLANIM detaylar icin ctrl+click yapip fonksiyon aciklamasini oku
             if (GameController.instance.score < 0) // SKOR SIFIRIN ALTINA DUSTUYSE
             {
@@ -319,19 +319,19 @@ public class PlayerController : MonoBehaviour
         else if (other.CompareTag("finish"))
         {
             other.GetComponent<Collider>().enabled = false;
-            if(transform.position.y != 0)MoveB();
+            if (transform.position.y != 0) MoveB();
             // GetComponent<Collider>().enabled = false;
         }
         else if (other.CompareTag("dance"))
-		{
+        {
             GameController.instance.isContinue = false;
             isEnableForSwipe = false;
             DanceAnim();
             UIController.instance.ActivateWinScreen();
             GameController.instance.ScoreCarp(1);
-		}
+        }
         else if (other.CompareTag("onboarding1") && PlayerPrefs.GetInt("level") < 4)
-		{
+        {
             KarakterPaketiMovement.instance._speed = 2;
             PlayerAnimator.speed = .2f;
             level1OnBoard.SetActive(true);
@@ -383,7 +383,7 @@ public class PlayerController : MonoBehaviour
     }
 
     void DeactivateOnboarding()
-	{
+    {
         KarakterPaketiMovement.instance._speed = 15;
         PlayerAnimator.speed = 1f;
         isEnableForSwipe = false;
@@ -394,27 +394,27 @@ public class PlayerController : MonoBehaviour
         level31OnBoard.SetActive(false);
         level32OnBoard.SetActive(false);
         level33OnBoard.SetActive(false);
-	}
+    }
 
     void CoinCrashEffects()
-	{
-		for (int i = 0; i < 5; i++)
-		{
-            GameObject coin = Instantiate(coinPrefab, transform.position, Quaternion.Euler(90,0,0));
-            coin.transform.localScale = new Vector3(40,40,40);
-            float x = Random.Range(-2,2);
-            float z = Random.Range(-1,2);
-            if (playerModel1.activeInHierarchy)coin.transform.DOJump(new Vector3(playerModel1Ayak.transform.position.x + x,playerModel1Ayak.transform.position.y, playerModel1Ayak.transform.position.z +z), 1, 1, .7f).SetEase(Ease.OutBounce).OnComplete(() => { Destroy(coin); });
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject coin = Instantiate(coinPrefab, transform.position, Quaternion.Euler(90, 0, 0));
+            coin.transform.localScale = new Vector3(40, 40, 40);
+            float x = Random.Range(-2, 2);
+            float z = Random.Range(-1, 2);
+            if (playerModel1.activeInHierarchy) coin.transform.DOJump(new Vector3(playerModel1Ayak.transform.position.x + x, playerModel1Ayak.transform.position.y, playerModel1Ayak.transform.position.z + z), 1, 1, .7f).SetEase(Ease.OutBounce).OnComplete(() => { Destroy(coin); });
             else if (playerModel2.activeInHierarchy) coin.transform.DOJump(new Vector3(playerModel2Ayak.transform.position.x + x, playerModel2Ayak.transform.position.y, playerModel2Ayak.transform.position.z + z), 1, 1, .7f).SetEase(Ease.OutBounce).OnComplete(() => { Destroy(coin); });
-        }    
-	}
+        }
+    }
 
     void GameOverEvents()
-	{
+    {
         GameController.instance.isContinue = false;
         IdleAnim();
         UIController.instance.ActivateLooseScreen();
-	}
+    }
 
     /// <summary>
     /// Bu fonksiyon her level baslarken cagrilir. 
@@ -431,7 +431,7 @@ public class PlayerController : MonoBehaviour
         GameController.instance.score = 0;
         transform.position = new Vector3(0, transform.position.y, 0);
         GetComponent<Collider>().enabled = true;
-        
+
     }
 
     public void PostStartingEvents()
@@ -443,20 +443,20 @@ public class PlayerController : MonoBehaviour
     }
 
     public void ChangePlayerModel()
-	{
-        if(LevelController.instance.totalLevelNo %2 == 0)
-		{
+    {
+        if (LevelController.instance.totalLevelNo % 2 == 0)
+        {
             playerModel1.SetActive(true);
             playerModel2.SetActive(false);
             PlayerAnimator = playerModel1.GetComponent<Animator>();
-		}
-		else
-		{
+        }
+        else
+        {
             playerModel1.SetActive(false);
             playerModel2.SetActive(true);
             PlayerAnimator = playerModel2.GetComponent<Animator>();
         }
-	}
+    }
 
 
 
